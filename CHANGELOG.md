28a8ce8eee1ed2c0fea7598b3c0808435e33de4f
jitendravaishnavs <jitendravaishnav763@gmail.com>
2025-07-19 20:42:57 +0530

    fix: ios compatibility issue resolved

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 ## 1.0.0
 
 - Upgraded to latest version of flutter
+
+## 1.1.0
+
+- Ios compatibility 
--- ios/Classes/EasyPdfViewerPlugin.m
+++ ios/Classes/EasyPdfViewerPlugin.m
@@ -1,11 +1,11 @@
 #import "EasyPdfViewerPlugin.h"
-#if __has_include(<easy_pdf_viewer/easy_pdf_viewer-Swift.h>)
-#import <easy_pdf_viewer/easy_pdf_viewer-Swift.h>
+#if __has_include(<jv_easy_pdf_viewer/jv_easy_pdf_viewer-Swift.h>)
+#import <jv_easy_pdf_viewer/jv_easy_pdf_viewer-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "easy_pdf_viewer-Swift.h"
+#import "jv_easy_pdf_viewer-Swift.h"
 #endif
 
 static NSString* const kDirectory = @"EasyPdfViewer";
